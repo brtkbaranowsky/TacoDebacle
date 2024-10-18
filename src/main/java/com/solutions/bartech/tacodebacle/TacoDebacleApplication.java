1e38606d4fd721b69a46b439ae1cd970ca91c880
Bartek <brtkbaranowski@gmail.com>
2024-10-18 18:39:48 +0200

    initial commit

---
+++ src/main/java/com/solutions/bartech/tacodebacle/TacoDebacleApplication.java
@@ -0,0 +1,13 @@
+package com.solutions.bartech.tacodebacle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoDebacleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoDebacleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solutions/bartech/tacodebacle/TacoDebacleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutions.bartech.tacodebacle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoDebacleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
